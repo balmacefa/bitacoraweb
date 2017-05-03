204983b58a5eb227a8981594f69e174ad845e29d
Fabián Balmaceda Rescia <fabianbalmaceda2@gmail.com>
2017-05-03 01:36:46 -0600

    cambiar con flechas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
   var lista = {};
   var lastActive;
+  var length;
+  var activeId = 0;
+
+  $(document).keydown(function(e) {
+    switch (e.which) {
+      case 37: // left
+        anterior();
+        break;
+
+      case 39: // right
+        siguiente();
+        break;
+
+      default:
+        return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+  });
+
+  function anterior() {
+    var newId = activeId - 1;
+    if (newId < 0) {
+      newId = length - 1;
+    }
+    cargarInfo(getItem(newId));
+  }
+
+  function siguiente() {
+    var newId = activeId + 1;
+    if (newId >= length) {
+      newId = 0;
+    }
+    cargarInfo(getItem(newId));
+  }
 
   function categoria(categoria) {
     return '<div class=\'categoria\'><h5>' + categoria + '</h5><div class=\'divider\'></div></div>';
@@ -21,56 +55,57 @@ $(document).ready(function() {
     }
     $('#lista-img').html(inHTML);
 
-    $('.item').on( 'click', function(e) {
-        e.preventDefault();
-        cargarInfo(getItem($(this).attr('id')));
+    $('.item').on('click', function(e) {
+      e.preventDefault();
+      cargarInfo(getItem($(this).attr('id')));
     });
 
     lastActive = $('#0');
     lastActive.toggleClass('active');
-    cargarInfo(getItem(0));
-}
+    cargarInfo(getItem(activeId));
+  }
 
-function getItem(id) {
-  for (var key in lista) {
-    for (var i = 0; i < lista[key].length; i++) {
-      if(lista[key][i].id == id){
-        return lista[key][i];
+  function getItem(id) {
+    for (var key in lista) {
+      for (var i = 0; i < lista[key].length; i++) {
+        if (lista[key][i].id == id) {
+          return lista[key][i];
+        }
       }
     }
   }
-}
 
-function cargarInfo(item) {
+  function cargarInfo(item) {
+    activeId = item.id;
+    lastActive.toggleClass('active');
+    lastActive = $('#' + item.id);
+    lastActive.toggleClass('active');
 
-  lastActive.toggleClass('active');
-  lastActive = $('#'+item.id);
-  lastActive.toggleClass('active');
+    $('#tituloDetalle').html(item.titulo);
+    $('#imgDetalle').attr('src', item.img);
+    $('#textoDetalle').html(item.descripcion);
 
-  $('#tituloDetalle').html(item.titulo);
-  $('#imgDetalle').attr('src', item.img);
-  $('#textoDetalle').html(item.descripcion);
-
-  $('#detalle').scrollTop(0);
-}
-
-$.ajax({
-  url: 'json/conceptos.json',
-  type: 'GET',
-  dataType: 'json',
-  success: function(data) {
-    for (var i = 0; i < data.length; i++) {
-      if (lista[data[i].categoria] === undefined) {
-        lista[data[i].categoria] = [];
-      }
-      data[i].id = i;
-      lista[data[i].categoria].push(data[i]);
-    }
-    llenarTabla();
-  },
-  error: function(jqXHR, textStatus, error) {
-    alert('error: ' + jqXHR.responseText);
+    $('#detalle').scrollTop(0);
   }
-});
+
+  $.ajax({
+    url: 'json/conceptos.json',
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      length = data.length;
+      for (var i = 0; i < data.length; i++) {
+        if (lista[data[i].categoria] === undefined) {
+          lista[data[i].categoria] = [];
+        }
+        data[i].id = i;
+        lista[data[i].categoria].push(data[i]);
+      }
+      llenarTabla();
+    },
+    error: function(jqXHR, textStatus, error) {
+      alert('error: ' + jqXHR.responseText);
+    }
+  });
 
 });
